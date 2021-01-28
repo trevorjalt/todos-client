cfc9d95f57cd9483b7dcf0a3bce5954f67093ffe
Trevor J Alt <trevor@trevorjalt.com>
2021-01-28 13:57:36 -0500

    component update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom';
 import CategoriesRoute from '../../Routes/CategoriesRoute';
 import NewTodoRoute from '../../Routes/NewTodoRoute';
+import TodosApiService from '../../Services/todos-api-service';
 import './App.css';
 
 class App extends Component {
+
+    state = {
+        error: null,
+        todos: [],
+    }
+
+    
+    componentDidMount() {
+      this.setState({ error: null })
+      TodosApiService.getTodos()
+          .then(this.setTodoList)
+    }
+
+
+    setTodoList = todoList => {
+        this.setState({ todoList })
+    }
+
+
     renderRoutes() {
         return (
             <Switch>
@@ -19,6 +39,7 @@ class App extends Component {
             </Switch>
         )
     }
+
 
     render() {
         return (
